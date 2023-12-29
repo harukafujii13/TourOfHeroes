5ec7d6a4e37ddf7310beff3b1e0b39c671c109ee
Haruka <hfujii1013@gmail.com>
2023-12-28 22:22:54 -0800

    feat: generate dashboard component

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+  imports: [
+    FormsModule,
+    NgIf,
+    NgFor,
+    UpperCasePipe,
+    HeroDetailComponent,
+    CommonModule,
+    RouterModule,
+  ],
+})
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService
+      .getHeroes()
+      .subscribe((heroes) => (this.heroes = heroes.slice(1, 5)));
+  }
+}
