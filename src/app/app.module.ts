7b4ef75d61b9da1522faab4b04004e410a111b85
Haruka <hfujii1013@gmail.com>
2023-12-28 21:05:26 -0800

    feat: import HeroDetailComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, HeroesComponent],
+  imports: [BrowserModule, FormsModule, HeroesComponent, HeroDetailComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
