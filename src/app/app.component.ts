42813bf9e7fa0ad5cb5b984bfa6c46697a52dc6a
Haruka <hfujii1013@gmail.com>
2023-12-28 20:35:11 -0800

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroe';
+  title = 'Tour of Heroes';
+}
+
+export class HeroesComponent {
+  heroes = HEROES;
 }
